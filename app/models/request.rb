55ecbfdd36fea48224675079e5f8aceeb259df4c
Kevin McCarthy <kjmagic13@gmail.com>
2016-04-23 22:27:49 -0400

    request field requirements

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,9 @@ class Request < ActiveRecord::Base
 
 	validates :service_id, :day, :name, :email, presence: true
 
+	def time(timezone='EDT')
+		set_hour = meridiem ? hour + 12 : hour
+		DateTime.new(day.year, day.month, day.day, set_hour, minute, 0, timezone)
+	end
+
 end
